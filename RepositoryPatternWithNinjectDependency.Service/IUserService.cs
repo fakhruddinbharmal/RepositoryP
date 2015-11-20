902ba639c0ac47591555155b8fd19c0d067be86b
Fakhruddin Bharmal <bharmalfakhruddin@gmail.com>
2015-11-20 18:51:12 +0530

    First Commit

---
+++ RepositoryPatternWithNinjectDependency.Service/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using RepositoryPatternWithNinjectDependency.core.Data;
+
+namespace RepositoryPatternWithNinjectDependency.Service
+{
+    public interface IUserService
+    {
+        IQueryable<User> GetUsers();
+        User GetUser(long id);
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+    }
+}
---
+++ RepositoryPatternWithNinjectDependency.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RepositoryPatternWithNinjectDependency.Service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("RepositoryPatternWithNinjectDependency.Service")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9af09870-5c6c-41e8-9ab2-0d44d9b57036")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
